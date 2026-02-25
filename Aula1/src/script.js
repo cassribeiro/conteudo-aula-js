58a4a7fcd5523f2343c0a9158664c801bfb83267
Cass Ribeiro <ribeirocassia2021@gmail.com>
2026-02-24 22:15:17 -0300

    Variáveis em JavaScript

---
+++ Aula1/src/script.js
@@ -0,0 +1,17 @@
+console.log("Testando JavaScript");
+
+//var nome = "Cass Ribeiro";
+
+//Exemplo de variável let
+let nome = "Cass Ribeiro";
+
+let nome2 = "Niall Horan";
+
+console.log(nome);
+
+console.log(nome2);
+
+//Exemplo de variável const 
+const bands = ["BTS", "One Direction", "The Rose"];
+
+console.log(bands);
