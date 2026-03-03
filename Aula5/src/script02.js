71c4cc969e2308a40ad5bc32c755096217a8209b
Cass Ribeiro <ribeirocassia2021@gmail.com>
2026-03-02 22:13:10 -0300

    Aula 5 - Laços de Repetição

---
+++ Aula5/src/script02.js
@@ -0,0 +1,14 @@
+//Exemplo do operador NOT
+let usuarioLogado = false;
+
+if (!usuarioLogado){
+    // console.log('teste')
+// } else{
+//     console.log('teste falso')
+}
+
+let lista = [];
+
+if (!lista.length) {
+    console.log('A lista está vazia');
+}
