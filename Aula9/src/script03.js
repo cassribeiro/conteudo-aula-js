96bee450030f3454cec3564b5a2dd152d643675e
Cass Ribeiro <ribeirocassia2021@gmail.com>
2026-03-09 21:12:14 -0300

    Aula - 09: BigInt em JS

---
+++ Aula9/src/script03.js
@@ -0,0 +1,25 @@
+//Utilizando BigInt
+let a = BigInt ('73648364346364864835683462560846235656145465740847964136497401471787365');
+console.log(a);
+
+//Utilizando n
+let b = 638265061235721657842759827959745993475954975928749754975789459827975812757349579487587475475947597457475n;
+console.log(b);
+
+//Utilizando operadores aritméticos com BigInt
+let c = 19543573654963594n;
+let d = 19543573654963594n;
+let e = (c * d);
+console.log(e);
+
+//Convertendo BigInt
+let f = BigInt('3976494665795296496478493429764792');
+let fconvertido = Number(f);
+console.log(fconvertido);
+
+//Utilizando métodos de comparação em BigInt
+let g = 100n;
+let h = 90n;
+let exComparacao = (g > h);
+console.log(exComparacao);
+
