d742a402a23b753ec1df359e1bcdc513dd2922de
Cass Ribeiro <ribeirocassia2021@gmail.com>
2026-03-16 20:47:47 -0300

    Aula - 14: Métodos de Busca em Array (Parte Final)

---
+++ Aula14/src/script03.js
@@ -0,0 +1,88 @@
+//Array global
+const frutas = ['Banana', 'Maçã', 'Morango', 'Uva', 'Laranja', 'Pêra'];
+
+//Utilizando o método sort()
+console.log(frutas);
+
+frutas.sort();
+
+console.log(frutas);
+
+//Utilizando o método reverse()
+frutas.reverse();
+
+console.log(frutas);
+
+//Combinando sort() e reverse()
+const frutas2 = ['Banana', 'Maçã', 'Morango', 'Uva', 'Laranja', 'Pêra'];
+console.log(frutas2);
+
+frutas2.sort();
+frutas2.reverse();
+
+console.log(frutas2);
+
+//Utilizando o método toSorted()
+const meses = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai'];
+
+const mesesSorted = meses.toSorted();
+
+console.log(meses);
+console.log(mesesSorted);
+
+//Utilizando o método toReversed()
+const mesesReversed = meses.toReversed();
+
+console.log(mesesReversed);
+
+//Utilizando o sort() em números 
+const numeros = [4,28,75,50,3,9,18];
+
+console.log(numeros);
+
+numeros.sort(function (a,b) {return a-b});
+
+console.log(numeros);
+
+
+const numeros2 = [4,28,75,50,3,9,18];
+
+console.log(numeros2);
+
+numeros2.sort(function (a,b) {return b - a});
+
+console.log(numeros2);
+
+//Utilizando o método sort() em números de maneira aleatória 
+const numeros3 = [4,28,75,50,3,9,18];
+
+numeros3.sort(function() {return 0.5 - Math.random()});
+
+//Utilizando o método Math.min()
+const numeros4 = [4,28,75,50,3,9,18];
+
+function menorNumeroArray(array){
+    return Math.min.apply(null,array)
+}
+
+console.log(menorNumeroArray(numeros4));
+
+//Utilizando o método Math.max em array
+function maiorNumeroArray(array){
+    return Math.max.apply(null, array)
+}
+
+console.log(maiorNumeroArray(numeros4));
+
+//Utilizando o método sort() com objetos 
+const pessoas = [
+    {nome: 'Cassia', idade: 23},
+    {nome: 'Louis', idade: 34},
+    {nome: 'Taehyung', idade: 30},
+];
+
+console.log(pessoas);
+
+pessoas.sort((a, b) => {return a.idade - b.idade});
+
+console.log(pessoas);
