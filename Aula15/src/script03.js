b42780f36ba14654499540e9992700e8f8005f92
Cass Ribeiro <ribeirocassia2021@gmail.com>
2026-03-18 22:11:11 -0300

    Aula- 15: Manipulações de Elementos no HTML

---
+++ Aula15/src/script03.js
@@ -0,0 +1,9 @@
+//Utilizando o innerHTML
+const elemento = document.querySelector('.mudandoElemento');
+
+elemento.innerHTML = 'Somente a dor coletiva gera união';
+
+//Alterando atributos
+const elemento1 = document.querySelector('.foto');
+
+elemento1.src = 'porquinho.jpeg';
