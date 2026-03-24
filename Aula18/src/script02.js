9562cfc9bcc1987001ff7eb88747d9b80fe76398
Cass Ribeiro <ribeirocassia2021@gmail.com>
2026-03-23 22:08:49 -0300

    Aula - 18: Métodos de Tempo

---
+++ Aula18/src/script02.js
@@ -0,0 +1,49 @@
+//Utilizando o método setInterval()
+const elemento = document.querySelector('.meu-relogio');
+
+setInterval(mostrarRelogio, 1000);
+
+function mostrarRelogio(){
+    const data = new Date();
+    const horas = data.getHours();
+    const minutos = data.getMinutes();
+    const segundos = data.getSeconds();
+
+    elemento.innerHTML = `${horas} : ${minutos} : ${segundos}`;
+};
+
+
+//Utilizando o método setTimeout()
+const conteudo = document.querySelector('.meu-conteudo');
+const botao = document.querySelector('.elemento-botao');
+
+botao.addEventListener('click', handlerTimeout);
+
+function handlerTimeout(){
+    setTimeout(mostrarconteudo, 2000);
+}
+
+function mostrarconteudo(){
+    conteudo.innerHTML =`Esse texto está aparecendo após 2 segundos da interação com o botão!`;
+};
+
+//Utilizando o método setInterval()
+const exibirNumeros = document.querySelector('.conteudo-contador');
+const botaoInicio = document.querySelector('.botao-inicio');
+const botaoPausar = document.querySelector('.botao-pausar');
+
+let intervalo;
+let contador = 0;
+
+botaoInicio.addEventListener('click', () => {
+    intervalo = setInterval(contadora, 1000);
+});
+
+botaoPausar.addEventListener('click', () => { 
+    clearInterval(intervalo)
+});
+
+function contadora() {
+    contador ++;
+    exibirNumeros.innerHTML = contador;
+};
